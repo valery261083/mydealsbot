546d2804501c4a303032187c654cb3b5f17c204f
valery261083 <33056169+valery261083@users.noreply.github.com>
2026-06-07 09:20:11 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,26 +1,33 @@
 import os
+import json
 import requests
 
 BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 CHANNEL = os.getenv("TELEGRAM_CHANNEL")
 
-text = """
-🔥 Знижка на Amazon.se
+with open("deals.json", "r", encoding="utf-8") as f:
+    deals = json.load(f)
 
-Товар: Приклад навушників
-Було: 999 kr
-Зараз: 299 kr
-Знижка: -70%
+deal = deals[0]
 
-👉 https://www.amazon.se/
+text = f"""
+🔥 {deal['title']}
+
+Було: {deal['old_price']}
+Зараз: {deal['new_price']}
+Знижка: {deal['discount']}
+
+👉 {deal['link']}
 """
 
 url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
 
-response = requests.post(url, json={
-    "chat_id": CHANNEL,
-    "text": text
-})
+requests.post(
+    url,
+    json={
+        "chat_id": CHANNEL,
+        "text": text
+    }
+)
 
-print("Status:", response.status_code)
-print("Response:", response.text)
+print("Deal sent")
